ba91dccb8cb700b8fe59c6752185812d8ae8098b
Artyom Tratsiuk <inqui05@gmail.com>
2020-11-19 16:54:48 +0300

    my homework to lesson 1

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert( "Hello friends!" );
+let money = '$';
+let income = 'salary';
+let addExpenses = 'taxes';
+let deposit = 1000;
+let mission = 1000000;
+const period = 2021;
+
+alert( `If you want to safe your ${money} you shell be able to withdraw them from the bank` );
+console.log( mission - deposit );
