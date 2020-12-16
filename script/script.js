9b3486c48ea3909a8120cd180251f60756f4f5a8
Artsiom Tratsiuk <inqui05@gmail.com>
2020-12-16 22:41:12 +0300

    my homework to lesson19

--- script/script.js
+++ script/script.js
@@ -1,7 +1,8 @@
 /* eslint-disable strict */
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
-    function countTimer(deadline) {
+    //таймер
+    const countTimer = deadline => {
         const timerHours = document.querySelector('#timer-hours'),
             timerMinutes = document.querySelector('#timer-minutes'),
             timerSeconds = document.querySelector('#timer-seconds');
@@ -36,14 +37,67 @@ window.addEventListener('DOMContentLoaded', () => {
                 timerMinutes.textContent = '00';
                 timerSeconds.textContent = '00';
             }
-
         }
 
         setInterval(updateClock, 1000);
-    }
-    countTimer('12/16/2020');
+    };
+    countTimer('12/17/2020');
 
+    //меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+            /*if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+                menu.style.transform = `translate(0)`;
+            } else {
+                menu.style.transform = `translate(-100%)`;
+            }*/
+            menu.classList.toggle('active-menu');
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+    };
+    toggleMenu();
+
+    //popup
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close'),
+            popupWindow = document.querySelector('.popup-content');
+        let request;
+        let count = 0;
+
+        const animatePopUp = function() {
+            request = requestAnimationFrame(animatePopUp);
+            count += 5;
+            if (count < 500) {
+                popupWindow.style.top = (-385 + count) + 'px';
+            } else {
+                count = 0;
+                cancelAnimationFrame(request);
+            }
+        };
+
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+                if (document.documentElement.clientWidth > 768) {
+                    popupWindow.style.top = '-385px';
+                    request = requestAnimationFrame(animatePopUp);
+                }
+            });
+        });
+
+        popUpClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+    };
+    togglePopUp();
 });
-
-
-
