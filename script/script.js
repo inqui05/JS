8f03cda78b0b00c4680c9e9bc92a68f7c5e33ee9
Tratsiuk Artsiom <inqui05l@gmail.com>
2020-12-23 21:02:14 +0300

    my homework to lesson24 + hard

--- script/script.js
+++ script/script.js
@@ -242,18 +242,80 @@ window.addEventListener('DOMContentLoaded', () => {
     slider();
 
     //калькулятор
-    const calculater = () => {
-        const calc = document.querySelector('.calc-block');
+    const calculater = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
 
-        calc.addEventListener('input', event => {
-            if (event.target.matches('.calc-square, .calc-count, .calc-day')) {
+        //честно позаимствованный блок с learn.javascript.ru для анимации
+        const animate = ({ timing, draw, duration }) => {
+            const start = performance.now();
+
+            requestAnimationFrame(function animate(time) {
+                let timeFraction = (time - start) / duration;
+                if (timeFraction > 1) {
+                    timeFraction = 1;
+                }
+
+                const progress = timing(timeFraction);
+
+                draw(progress);
+
+                if (timeFraction < 1) {
+                    requestAnimationFrame(animate);
+                }
+            });
+        };
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            animate({
+                duration: 1000,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    totalValue.textContent = Math.floor(progress * total);
+                },
+            });
+        };
+
+        calcBlock.addEventListener('change', event => {
+            const target = event.target;
+            if (target.matches('.calc-square, .calc-count, .calc-day')) {
                 event.target.value = event.target.value.replace(/\D/g, '');
+            }
+
+            if (target.matches('.calc-type, .calc-square, .calc-count, .calc-day')) {
+                countSum();
             }
         });
     };
     calculater();
 
-    //пункт 1 домашки. У многих изображений не прописан data-img, поэтому для них ничего не будет меняться
+    //меняем изображения
     const numberOne = () => {
         const body = document.querySelector('body');
 
